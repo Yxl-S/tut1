99d73523f744075a8af734fa9747b7023ee9802f
Yxl-S <Y1039155192@outlook.com>
2025-09-21 19:30:13 +0800

    Added feature 2

--- Greeter.java
+++ Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
 
   public String sayHello() {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
-    return messages.getString("greeting") + ", " + name;
+    return messages.getString("greeting") + ", " + name.toUpperCase(); // ✅ name 变成大写
   }
 
   public static void main(String[] args) {
